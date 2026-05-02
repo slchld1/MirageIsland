1388ef62100c8aa19cf8793418e2750bd1a80740
Minjae Cho <jaecho203@gmail.com>
2026-05-02 18:59:13 -0500

    feat(fishing): add waitHookMultiplier + reelHookMultiplier to Bait

--- Assets/Scripts/Fishing/Bait.cs
+++ Assets/Scripts/Fishing/Bait.cs
@@ -1,4 +1,14 @@
+using UnityEngine;
+
 public class Bait : Item
 {
     public BaitType baitType;
+
+    [Header("Hook chance multipliers")]
+    [Tooltip("Multiplier on per-second hook chance while lure is stationary")]
+    public float waitHookMultiplier = 1.0f;
+
+    [Tooltip("Multiplier on per-pixel hook chance while reeling")]
+    public float reelHookMultiplier = 1.0f;
+
 }
